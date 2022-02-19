f731c6a088686e46d8e3b0069bc92708d66d3e04
David Ioner <davidhioner@gmail.com>
2022-02-19 20:28:19 -0300

    db created

---
+++ components/send_pdf.py
@@ -0,0 +1,52 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+def send_email(attachment, to='Nigel@NSNYRE.com'):
+    host = "smtp.gmail.com"
+    port = '587'
+    login = '5CRE.contracts@gmail.com'
+    password = ''
+
+    server = smtplib.SMTP(host, port)
+    server.ehlo()
+    server.starttls()
+    server.login(login, password)
+
+    # separete filename from path
+    filename = attachment.split('/')[-1]
+    name = filename.split('.')[0]
+    name.replace('CRE_', '')
+    # create email
+    body = """
+    <p>Hi,</p>
+    <p>this is an automatic message!</p>
+    <p>Created by: <b>David Ioner</b></p>
+    <p>
+    """
+
+    mail_msg = MIMEMultipart()
+    mail_msg['From'] = login
+    mail_msg['To'] = to
+    mail_msg['Subject'] = f'{name} contract'
+    mail_msg.attach(MIMEText(body, 'html'))
+
+    attach = open(attachment, 'rb')
+
+    att = MIMEBase('application', 'octet-stream')
+    att.set_payload(attach.read())
+    encoders.encode_base64(att)
+
+
+    
+    att.add_header('Content-Disposition', 'attachment', filename=filename)
+    attach.close()
+    mail_msg.attach(att)
+
+    #send email
+    server.sendmail(mail_msg['From'], mail_msg['To'], mail_msg.as_string())
+    server.quit()
--- main.py
+++ main.py
@@ -1,48 +1,63 @@
-from kivy.lang import Builder # Builder necessário para utilizar o Builder.load_string
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
+from kivymd.app import MDApp
+# import components.to_pdf_class as pdf
+import components.DButil as dbutil
+
 from kivy.metrics import dp
 from kivymd.uix.menu import MDDropdownMenu
-from kivymd.app import MDApp
-
-# Nesse caso, as classes devem estender Screen
-
-class CustomerScreen(Screen):
-
-	def to_worker(self):
-		self.manager.current = 'worker_screen'
-
-class WorkerScreen(Screen):
-	# Chama outra tela que contém o menu e uma imagem do logotipo
-	def to_customer(self):
-		self.manager.current = 'customer_screen'
-
-class MyScreenManager(ScreenManager):
-    pass
 
 
 class App(MDApp):
-	def __init__(self,*args, **kwargs):
-		super().__init__(*args,**kwargs)
-		self.kv = Builder.load_file('ui.kv')
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.kv = Builder.load_file('components/ui.kv')
 		menu_items = [
             {
-                "text": f"Item {i}",
-                "viewclass": "OneLineListItem",
-                "on_release": lambda x=f"Item {i}": self.menu_callback(x),
-            } for i in range(5)
+                "viewclass": "OneLineIconListItem",
+                "text": f"local 1",
+                "height": dp(56),
+                "on_release": lambda x=f"#1 2DA CERRADA LAGO BOLSENA": self.set_item(x),
+            },
+            {
+                "viewclass": "OneLineIconListItem",
+                "text": f"local 2 (not available)",
+                "height": dp(56),
+                "on_release": lambda x=f"#2": self.set_item(x),
+            }, 
+            {
+                "viewclass": "OneLineIconListItem",
+                "text": f"local 3 (not available)",
+                "height": dp(56),
+                "on_release": lambda x=f"#3": self.set_item(x),
+            } 
         ]
 		self.menu = MDDropdownMenu(
-			
+            caller=self.kv.ids.drop_local,
             items=menu_items,
+            position="center",
             width_mult=4,
         )
-
-	def menu_callback(self, text_item):
+        
+	def set_item(self, text_item):
+		self.kv.ids.drop_local.set_item(text_item)
+		self.menu.dismiss()
 		print(text_item)
-
+		if "#1" in text_item:
+			self.specific_location = "2DA CERRADA LAGO BOLSENA #54, LAGO NORTE DF, MIGUEL HIDALGO, DF. MX"
+			self.location = "Mexico City, Mexico"
+			self.city = "Mexico City"
+		else:
+			self.specific_location = text_item
+	
+	def submit(self):
+		# dbutil.insert_data(
+		pass
+        
+	def handle_key(self):
+		pass
 
 	def build(self):
-		return MyScreenManager
+		return self.kv
 		
 
 App().run()
